d2b0de298d9a6c4c078a041a433314d05d39c91f
Frank Moreno <Frank.Moreno95@gmail.com>
2022-08-14 11:03:56 -0500

    Get client context

--- netlify/functions/test/main.go
+++ netlify/functions/test/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"context"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	// Your server-side functionality
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, Frank!",
+		Body:       "Hello, " + cc.Client.AppTitle,
 	}, nil
 }
 
